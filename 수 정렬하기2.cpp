c1990b4ed65af8a35f3c4084ae95169be93000ca
Jaemin <69967974+als982001@users.noreply.github.com>
2021-06-06 14:57:09 +0900

    Create 수 정렬하기2.cpp
    문제
    N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 숫자가 주어진다. 이 수는 절댓값이 1,000,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
    
    출력
    첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
    
    예제 입력 1
    5
    5
    4
    3
    2
    1
    예제 출력 1
    1
    2
    3
    4
    5

---
+++ 수 정렬하기2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int arr[1000000];
+
+int main()
+{
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; ++i)
+		scanf("%d", &arr[i]);
+
+	sort(arr, arr + n);
+
+	for (int i = 0; i < n; ++i)
+		printf("%d\n", arr[i]);
+
+	return 0;
+}
