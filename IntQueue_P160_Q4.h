35c94034b7ecdabace2a1594a166fed9529af9ca
Jaemin <69967974+als982001@users.noreply.github.com>
2021-06-06 14:39:02 +0900

    P160 Q4
    int형 큐를 사용하는 프로그램에 임의의 데이터를 검색하는 아래의 함수를 추가하세요.
    
    int Search2(const IntQueue* q, int x);

---
+++ IntQueue_P160_Q4.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#ifndef ___IntQueue
+#define ___IntQueue
+
+typedef struct
+{
+	int max;		// 큐의 최대 용량
+	int num;		// 현재의 요소 개수
+	int front;		// 프런트
+	int rear;		// 리어
+	int* que;		// 큐 맨 앞 요소에 대한 포인터
+}IntQueue;
+
+// 큐 초기화
+int Initialize(IntQueue* q, int max);
+
+// 큐에 데이터를 인큐
+int Enqueue(IntQueue* q, int x);
+
+// 큐에 데이터를 디큐
+int Dequeue(IntQueue* q, int* x);
+
+// 큐에 데이터를 피크
+int Peek(const IntQueue* q, int* x);
+
+// 모든 데이터 삭제
+void Clear(IntQueue* q);
+
+// 큐의 최대 용량
+int Capacity(const IntQueue* q);
+
+// 큐에 저장된 데이터 개수
+int Size(const IntQueue* q);
+
+// 큐가 비어 있는가?
+int IsEmpty(const IntQueue* q);
+
+// 큐가 가득 찼는가?
+int IsFull(const IntQueue* q);
+
+// 큐에서 검색
+int Search(const IntQueue* q, int x);
+
+// 모든 데이터 출력
+void Print(const IntQueue* q);
+
+// 큐 종료
+void Terminate(IntQueue* q);
+
+// 임의의 데이터 검색
+int Search2(const IntQueue* q, int x);
+
+#endif
