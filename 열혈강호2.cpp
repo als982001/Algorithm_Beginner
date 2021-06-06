2ac4d039fcbeec709892131c1f46da3c439a8e90
Jaemin <69967974+als982001@users.noreply.github.com>
2021-06-06 14:59:43 +0900

    Create 열혈강호2.cpp
    문제
    강호네 회사에는 직원이 N명이 있고, 해야할 일이 M개가 있다. 직원은 1번부터 N번까지 번호가 매겨져 있고, 일은 1번부터 M번까지 번호가 매겨져 있다.
    
    각 직원은 최대 두 개의 일을 할 수 있고, 각각의 일을 담당하는 사람은 1명이어야 한다.
    
    각각의 직원이 할 수 있는 일의 목록이 주어졌을 때, M개의 일 중에서 최대 몇 개를 할 수 있는지 구하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 직원의 수 N과 일의 개수 M이 주어진다. (1 ≤ N, M ≤ 1,000)
    
    둘째 줄부터 N개의 줄의 i번째 줄에는 i번 직원이 할 수 있는 일의 개수와 할 수 있는 일의 번호가 주어진다.
    
    출력
    첫째 줄에 강호네 회사에서 할 수 있는 일의 개수를 출력한다.
    
    예제 입력 1
    5 5
    2 1 2
    2 1 2
    2 1 2
    2 4 5
    0
    예제 출력 1
    4

---
+++ 열혈강호2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#define MAX 1001
+
+using namespace std;
+
+vector<int> a[MAX];
+bool check[MAX];
+int pointed[MAX];
+
+bool dfs(int s)
+{
+	for (int i = 0; i < a[s].size(); ++i)
+	{
+		int t = a[s][i];
+
+		if (check[t])
+			continue;
+		check[t] = true;
+
+		if (pointed[t] == 0 || dfs(pointed[t]))
+		{
+			pointed[t] = s;
+			return true;
+		}
+	}
+	return false;
+}
+
+int main()
+{
+	int n, m;
+
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		int t, cs;
+		cin >> t;
+		for (int j = 1; j <= t; ++j)
+		{
+			cin >> cs;
+			a[i].push_back(cs);
+		}
+	}
+
+	int count = 0;
+	for (int i = 0; i < 2; ++i)
+	{
+		for (int j = 1; j <= n; ++j)
+		{
+			fill(check, check + MAX, false);
+			if (dfs(j))
+				++count;
+		}
+	}
+
+	cout << count << endl;
+	return 0;
+}
